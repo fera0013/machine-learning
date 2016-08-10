4c35a1aa3057e4ee728a80a25fe3b7791d9a5d4b
fera0013 <ralphfehrer@gmail.com>
2016-08-10 11:41:23 +0200

    Update agent.py
    First complete approach

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -2,6 +2,8 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+import numpy as np
+import itertools
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
@@ -11,29 +13,60 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
-
+        self.valid_actions = [None, 'forward', 'left', 'right']
+        self.valid_TraficLights = ['green','red'] 
+        self.stateSpace  = {'light': self.valid_TraficLights, 'oncoming': self.valid_actions, 'left': self.valid_actions, 'right': self.valid_actions}
+        self.Q={}
+        self.alpha=0
+        self.gamma=0
+        self.previousState=None
+        self.previousAction=None
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
-
+        self.reset_Q_table(self.stateSpace,  self.valid_actions)
+        self.alpha=0
+        self.gamma=0
+        self.previousState=None
+        self.previousAction=None
+    def choose_action(self, state):
+        assert state in self.Q.keys(), "Unknown state!"
+        QValues=list(self.Q[state].values())
+        actions=list(self.Q[state].keys())
+        actionsWithMaxQ = [action for action in actions if self.Q[state][action] == max(QValues)]
+        action =  random.choice(actionsWithMaxQ)
+        return action
+    def get_Q_value(self, state,action):
+        assert state in self.Q.keys(), "Unknown state!"
+        return self.Q[state][action]
+    def reset_Q_table(self,states,actions):
+        listOfPossibleStateValues = [states[stateCategory] for stateCategory in states.keys()]
+        actionsDict=dict(zip(actions,[0]*len(actions)))
+        stateID={}
+        for state in itertools.product(*listOfPossibleStateValues):
+            for i,partState in enumerate(state):
+                stateID[states.keys()[i]]=partState
+            self.Q[str(stateID)]=actionsDict.copy()
     def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
-
         # TODO: Update state
-        
+        self.state=inputs
         # TODO: Select action according to your policy
-        action = None
-
+        stateID=str(self.state)
+        action = self.choose_action(stateID)
         # Execute action and get reward
         reward = self.env.act(self, action)
-
         # TODO: Learn policy based on state, action, reward
-
+        if self.previousAction is not None and self.previousState is not None:
+            self.alpha=1/2
+            self.gamma=1/2
+            self.Q[self.previousState][self.previousAction]=self.alpha*(reward+self.gamma*max([self.Q[ stateID][action] for action in self.env.valid_actions])) 
+        self.previousAction = action
+        self.previousState = stateID  
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
-
 
 def run():
     """Run the agent for a finite number of trials."""
@@ -43,7 +76,7 @@ def run():
     a = e.create_agent(LearningAgent)  # create agent
     e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
-
+    e.enforce_deadline=False
     # Now simulate it
     sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
