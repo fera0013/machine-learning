baa479c38fee701caa08ac04c26932fadb9cea15
fera0013 <ralphfehrer@gmail.com>
2016-08-26 12:35:07 +0200

    solution, 1st approach

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -25,6 +25,7 @@ class LearningAgent(Agent):
         self.number_of_trials=0
         self.successful_trials=[]
         self.number_of_driving_errors=0
+        self.driving_errors=[]
     def reset(self, destination=None):
         self.planner.route_to(destination)
         self.previous_state=None
@@ -32,6 +33,7 @@ class LearningAgent(Agent):
         self.previous_reward=None
         self.number_of_trials+=1
         self.successful_trials.append(self.number_of_successful_trials)  
+        self.driving_errors.append(self.number_of_driving_errors)
         self.number_of_driving_errors=0
     def get_epsilon(self):
         return (float)(1)/(self.number_of_trials)
@@ -64,7 +66,7 @@ class LearningAgent(Agent):
         self.next_waypoint = self.planner.next_waypoint()  
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
-        self.state=str(inputs)
+        self.state=str((inputs,self.next_waypoint))
         action = self.choose_next_action(self.state)
         reward = self.env.act(self, action)
         if reward>0: self.number_of_driving_errors+=1 
@@ -76,16 +78,17 @@ class LearningAgent(Agent):
         self.previous_reward = reward
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
         self.calculate_statistics()
-def display_evaluation_statistics(best_candidates,figure_name='stats'):
-    fig = plt.figure()
-    fig.canvas.set_window_title(figure_name) 
-    ax = fig.add_subplot(111)
-    for candidate in best_candidates:
-        ax.plot(candidate.successful_trials,label='alpha: '+str(candidate.alpha)+' gamma: '+str(candidate.gamma))
-    ax.set_xlabel('trial')
-    ax.set_ylabel('succesful trials')
-    #ax.set_title=('Test')
-    ax.legend(loc = 'best').draggable()
+def display_evaluation_statistics(agents,figure_name='stats'):
+    num_axis=int(math.sqrt(len(agents)))
+    f, axarr = plt.subplots(num_axis,num_axis)
+    f.suptitle(figure_name)
+    for i,agent in enumerate(agents):
+        y=i%num_axis
+        x=(i-y)/num_axis
+        axarr[x, y].plot(agent.successful_trials)
+        axarr[x, y].plot(agent.driving_errors)
+        axarr[x,y].set_title('alpha: '+str(agent.alpha)+' gamma: '+str(agent.gamma))
+        axarr[x,y].legend(['#succesfull','#driving errors'])
     plt.show()
 def run_with_random_actions():
     e = Environment() 
@@ -100,9 +103,9 @@ def run_with_random_actions():
     sim.run(n_trials=100)  
     display_evaluation_statistics([a],"With random action selection")
 def run_with_q_learning():
-    best_candidates=[]
-    for alpha in np.arange(0.1,1,0.1):
-        for gamma in np.arange(0.1,1,0.1):
+    agents=[]
+    for alpha in np.arange(0.1,0.9,0.2):
+        for gamma in np.arange(0.1,0.9,0.2):
             e = Environment()  
             a = e.create_agent(LearningAgent) 
             e.set_primary_agent(a, enforce_deadline=True)  
@@ -113,24 +116,8 @@ def run_with_q_learning():
             a.alpha=alpha
             sim = Simulator(e, update_delay=0, display=False)  
             sim.run(n_trials=100) 
-            if len(best_candidates)<5:
-                best_candidates.append(a)
-            else:
-                min_candidate = min([(i,candidate.successful_trials[-1]) for i,candidate in enumerate(best_candidates)],
-                                    key=lambda t:t[1])
-                if a.successful_trials[-1]>min_candidate[1]:
-                    best_candidates[min_candidate[0]]=a
-    display_evaluation_statistics(best_candidates,"100 trials for all parameter combinations with Q-learning")
-    for i,candidate in  enumerate(best_candidates):
-        e = Environment()  
-        a = e.create_agent(LearningAgent) 
-        e.set_primary_agent(a, enforce_deadline=True)  
-        a.gamma=candidate.gamma
-        a.alpha=candidate.alpha
-        sim = Simulator(e, update_delay=0, display=False)  
-        sim.run(n_trials=1000) 
-        best_candidates[i]=a
-    display_evaluation_statistics(best_candidates,"1000 trials for n best agents with Q-learning")
+            agents.append(a)
+    display_evaluation_statistics(agents,"100 trials for all parameter combinations with Q-learning")
 def run():
     #run_with_random_actions()
     run_with_q_learning()
